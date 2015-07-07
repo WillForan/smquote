372ca20a198fe7dfdca74bef5e5c710e59d0dbb6
Will Foran <willforan@gmail.com>
2015-07-07 19:58:32 -0400

    use sql_src

--- production/screen.R
+++ production/screen.R
@@ -5,14 +5,16 @@ library(dplyr)
 library(DBI)
 library(smquote)
 
-con <- dbConnect(RSQLite::SQLite(),"stock.db")
-quotesdf <- dbFetch(res <- dbSendQuery(con,'select * from history'))
-dbClearResult(res)
-# "sym" becomes "Name"
+#con <- dbConnect(RSQLite::SQLite(),"stock.db")
+#quotesdf <- dbFetch(res <- dbSendQuery(con,'select * from history'))
+#dbClearResult(res)
+#dbDisconnect(con)
+
+quotesdf <- as.data.frame(tbl(sqldb <- src_sqlite(path = "stock.db"),"history"))
+
+# "sym" becomes "Name", "day" -> "Date", etc
 names(quotesdf) <- c("Name","Date","Open","Close","High","Low","Volume")
 quotesdf$Adj.Close <- quotesdf$Close
 quotesdf$Date <- as.Date(quotesdf$Date)
 alldf <- make.history(quotesdf,20)
 
-
-dbDisconnect(con)
