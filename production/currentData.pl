f78be6e68d3d9fcd70a0f9fac829cb3309ba6a59
Will Foran <wforan1@andrew.cmu.edu>
2015-07-09 07:29:19 -0400

    currentData can work on its own output, demonstrate in makefile

--- production/currentData.pl
+++ production/currentData.pl
@@ -7,9 +7,17 @@ my $q=Finance::Quote->new();
 
 # Name    low.1sd   Volume        slope  Close
 my $header=<>; chomp $header;
-my @header=('row',split/\s+/,$header);
+my @header=(split/\s+/,$header);
 
-say join("\t",'Name','CurPrice',@header[2..$#header]);
+# bail if we've run this too many times
+my $runno= scalar(grep {/CurPrice/} @header);
+exit if $runno >1;
+
+# we can feed the results of this back into itself
+# to get an update
+@header = map {s/CurPrice/BuyPrice/;$_} @header;
+
+say join("\t",'Name','CurPrice',@header[1..$#header]);
 while($_ = <>) {
   chomp;
   my %l;
@@ -21,5 +29,5 @@ while($_ = <>) {
   #say Dumper($s);
   my $price = $s->{$l{Name},'ask'} || $s->{$l{Name},'last'};
   # should buy if ask price is less than 1 sd below mean
-  say join("\t",$l{Name},$price,@l{@header[2..$#header]}) if $price < $l{'low.1sd'};
+  say join("\t",$l{Name},$price,@l{@header[1..$#header]}) if $price < $l{'low.1sd'} || $runno > 0;
 }
